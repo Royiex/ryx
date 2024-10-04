a98c8ab70db73d19192571afb7f246200ef9e60d
Royex <royex@royex>
2024-10-04 19:55:50 +0200

    print 'hello world' on startup

--- ryx.c
+++ ryx.c
@@ -171,9 +171,18 @@ if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
 }
 
 /*** file io ***/
-
 //basic file input/output
 
+void editorOpen() {
+    char *line = "Hello, world!";
+    ssize_t linelen = 13;
+
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen +1);
+    memcpy(E.row.chars, line, linelen);
+    E.row.chars[linelen] = '\0';
+    E.numrows = 1;
+}
 
 /*** append buffer ***/
 struct abuf {
@@ -204,26 +213,32 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
     int y;
     for (y=0; y < E.screenrows; y++) {
+        if (y >= E.numrows) {
+            //print welcome message
+            if (y == E.screenrows / 3) {
+                char welcome[80];
+                int welcomelen = snprintf(welcome, sizeof(welcome),
+                    "by royex -- ryx editor -- version %s", RYX_VERSION);
+                if (welcomelen > E.screencols) welcomelen = E.screencols;
 
-        //print welcome message
-        if (y == E.screenrows / 3) {
-            char welcome[80];
-            int welcomelen = snprintf(welcome, sizeof(welcome),
-                "by royex -- ryx editor -- version %s", RYX_VERSION);
-            if (welcomelen > E.screencols) welcomelen = E.screencols;
+                //center the text
+                int padding = (E.screencols - welcomelen) / 2;
+                if (padding) {
+                    abAppend(ab, "~", 1);
+                    padding--;
+                }
+                while (padding--) abAppend(ab, " ", 1);
 
-            //center the text
-            int padding = (E.screencols - welcomelen) / 2;
-            if (padding) {
+                abAppend(ab, welcome, welcomelen);
+            } else {
                 abAppend(ab, "~", 1);
-                padding--;
             }
-            while (padding--) abAppend(ab, " ", 1);
-
-            abAppend(ab, welcome, welcomelen);
         } else {
-            abAppend(ab, "~", 1);
+            int len = E.row.size;
+            if (len > E.screencols) len = E.screencols;
+            abAppend(ab, E.row.chars, len);
         }
+
         abAppend(ab, "\x1b[K", 3);
         if (y < E.screenrows - 1) {
             abAppend(ab, "\r\n", 2);
@@ -321,6 +336,7 @@ void initEditor() {
 int main() {
     enableRawMode();
     initEditor();
+    editorOpen();
 
     while(1) {
         editorRefreshScreen();
